caea3212d8e8dfffdaa3e35b1c9e7bcc344df063
Drake Nelson <drake.aren@pm.me>
2022-04-25 01:32:46 +0600

    Add 3xl, 4xl resolutions

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,19 @@ module.exports = {
     `plugins/**/*.{js,ts}`,
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm':  '640px',  // => @media (min-width: 640px) { ... }
+      'md':  '768px',  // => @media (min-width: 768px) { ... }
+      'lg':  '1024px', // => @media (min-width: 1024px) { ... }
+      'xl':  '1280px', // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px', // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      screens: {
+        '3xl': '1600px',
+        '4xl': '1920px',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
