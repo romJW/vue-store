32b219040312543cec55fecb8eb8b6db58688077
Drake Nelson <drake.aren@pm.me>
2022-04-25 01:32:35 +0600

    Fix up router overrides

--- nuxt.config.js
+++ nuxt.config.js
@@ -101,16 +101,4 @@ export default {
       }
     }
   },
-
-  router: {
-    scrollBehavior(to) {
-      if (to.hash) {
-        return window.scrollTo({
-          top: document.querySelector(to.hash).offsetTop + window.innerHeight,
-          behavior: 'smooth'
-        })
-      }
-      return window.scrollTo({ top: 0, behavior: 'smooth' })
-    }
-  },
 }
