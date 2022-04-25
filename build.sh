13627223988e78ce6b773cca8a7024960bea59b5
Drake Nelson <drake.aren@pm.me>
2022-04-25 12:01:44 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-DOMAIN=$1
-VERSION=$2
-TAG=registry.gitlab.com/croxware/ecom.client/$DOMAIN:$VERSION
+VERSION=$1
+TAG=registry.gitlab.com/croxware/ecom.client:$VERSION
 
 docker build -t $TAG .
 docker push $TAG
