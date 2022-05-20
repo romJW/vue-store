185f6203abd493440a7a8cfcebc97bba5c66f458
Drake Nelson <drake.aren@pm.me>
2022-05-20 20:08:25 +0600

    Migrate static pages to use graphql

--- pages/page/_url.vue
+++ pages/page/_url.vue
@@ -1,22 +1,41 @@
 <template>
-  <Loading v-if="loading" />
+  <Loading v-if="$apollo.loading" />
   <div v-else class="container mx-auto flex justify-center">
     <div class="prose max-w-none w-[60vw]" v-html="page.content" />
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
+import gql from 'graphql-tag'
 import Loading from '@/library/Loading.vue'
 
 export default {
   components: {
     Loading,
   },
-  data() {
-    return {
-      loading: true,
-      page: null
-    }
+  apollo: {
+    page: {
+      query: gql`
+        query getStaticPage($id: String!) {
+          static_pages(filter: { id: { _eq: $id} }) {
+            id
+            content
+            seo_title
+            seo_description
+            seo_tags
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.$route.params.url
+        }
+      },
+      update: data => {
+        return _.head(data.static_pages)
+      },
+    },
   },
   head() {
     if (this.page) {
@@ -27,19 +46,15 @@ export default {
             hid: 'description',
             name: 'description',
             content: this.page.seo_description,
-          }
+          },
+          {
+            hid: 'keywords',
+            name: 'keywords',
+            content: this.page.seo_tags,
+          },
         ]
       }
     }
   },
-  mounted() {
-    this.$axios.get(`/directus/items/static_pages/${this.$route.params.url}`)
-      .then(data => {
-        this.page = data.data.data
-      })
-      .finally(() => {
-        this.loading = false
-      })
-  }
 }
 </script>
