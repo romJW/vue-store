e2d8b94523691400dcbcee330753441bb7028a71
Drake Aren <drake.aren@protonmail.com>
2022-06-02 14:34:32 +0600

    Add Popover component to library

---
+++ library/Popover.vue
@@ -0,0 +1,18 @@
+<template>
+  <div @mouseover="isHovered = true" @mouseleave="isHovered = false">
+    <slot/>
+    <span v-if="isHovered" class="absolute shadow-md z-50 bg-white p-4 max-w-md">
+      <slot name="content"/>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isHovered: false
+    }
+  }
+}
+</script>
