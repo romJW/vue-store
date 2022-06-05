3ce7daa8b7f7168a9a0f7611744cf1b1a787eb82
Drake Aren <drake.aren@protonmail.com>
2022-06-05 21:12:11 +0600

    Add hero video

--- components/index/Hero.vue
+++ components/index/Hero.vue
@@ -1,10 +1,15 @@
 <template>
   <aside class="relative text-white bg-sky-900">
-    <img
-      src="@/assets/hero.jpg"
-      alt=""
-      class="absolute inset-0 object-cover w-full h-full brightness-50"
-    />
+    <video
+      class="absolute inset-0 object-cover w-full h-full brightness-80"
+      src="@/assets/video/hero.mp4"
+      autoplay
+      poster
+      muted
+      loop
+    >
+      <img src="@/assets/hero.jpg" alt="" />
+    </video>
     <div class="relative max-w-screen-xl px-4 py-24 lg:py-36 2xl:py-52 mx-auto sm:px-6 lg:px-8">
       <div class="flex flex-col max-w-3xl mx-auto text-center gap-8">
         <h1 class="mt-2 text-4xl font-bold text-white sm:text-6xl">
