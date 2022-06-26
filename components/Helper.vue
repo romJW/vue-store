787ceb56e27f138cd1c76797f1892c61b6e35f06
Roman Bukharin <buxarin1999@gmail.com>
2022-06-26 16:32:08 +0600

    add some to question mark icons

--- components/Helper.vue
+++ components/Helper.vue
@@ -1,7 +1,7 @@
 <template>
   <Popper :options="options">
     <slot slot="content" />
-      <fa-icon icon="fa-regular fa-circle-question"   slot="reference" class="material-icons text-blue-500 w-6 h-6 "/>
+      <fa-icon icon="fa-regular fa-circle-question "   slot="reference" class="material-icons text-blue-500 w-6 h-6 mb-1.5"/>
     
   </Popper>
 </template>
